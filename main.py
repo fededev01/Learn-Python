bd491eeaa3b56588b066e075eaa51468467cd8ce
Federico <45139706+fededev01@users.noreply.github.com>
2020-05-07 23:56:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#This repo will teach you the programming language called Python
+import datetime
+
+now = datetime.datetime.now()
+print("Current date and time: ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print("Congratulation, this is your first Python program")
